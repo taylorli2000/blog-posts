b6c6d48a29a37a98a7b6d63c0a1229da410babf9
taylor <t4ylor.li@gmail.com>
2021-10-19 09:57:59 -0400

    formatting

--- api/getBlogPosts.test.js
+++ api/getBlogPosts.test.js
@@ -1,38 +1,38 @@
-import { describe } from "jest-circus";
-import request from "supertest";
-import app from "../index.js";
+import { describe } from 'jest-circus';
+import request from 'supertest';
+import app from '../index.js';
 
-describe("Test posts path with no query parameters", () => {
-  test("status code should be 400 with error message", async () => {
-    const response = await request(app).get("/api/posts");
+describe('Test posts path with no query parameters', () => {
+  test('status code should be 400 with error message', async () => {
+    const response = await request(app).get('/api/posts');
     expect(response.statusCode).toBe(400);
     expect(response.body.error).toEqual(
-      expect.stringContaining("Tags parameter is required")
+      expect.stringContaining('Tags parameter is required')
     );
   });
 });
 
-describe("Test posts path with valid tags parameter", () => {
-  test("status code should be 200 with posts returned", async () => {
-    const response = await request(app).get("/api/posts?tags=history,tech");
+describe('Test posts path with valid tags parameter', () => {
+  test('status code should be 200 with posts returned', async () => {
+    const response = await request(app).get('/api/posts?tags=history,tech');
     expect(response.statusCode).toBe(200);
     expect(response.body.posts[0].id).toEqual(1);
     expect(response.body.posts[response.body.posts.length - 1].id).toEqual(100);
   });
 });
 
-describe("Test posts path with invalid tags parameter", () => {
-  test("status code should be 200 with no posts returned", async () => {
-    const response = await request(app).get("/api/posts?tags=abc");
+describe('Test posts path with invalid tags parameter', () => {
+  test('status code should be 200 with no posts returned', async () => {
+    const response = await request(app).get('/api/posts?tags=abc');
     expect(response.statusCode).toBe(200);
     expect(response.body.posts.length).toEqual(0);
   });
 });
 
-describe("Test posts path with valid tags parameter and valid sortBy", () => {
-  test("status code should be 200 with posts sorted by likes", async () => {
+describe('Test posts path with valid tags parameter and valid sortBy', () => {
+  test('status code should be 200 with posts sorted by likes', async () => {
     const response = await request(app).get(
-      "/api/posts?tags=history,tech&sortBy=likes"
+      '/api/posts?tags=history,tech&sortBy=likes'
     );
     expect(response.statusCode).toBe(200);
     expect(response.body.posts[0].likes).toEqual(25);
@@ -42,22 +42,22 @@ describe("Test posts path with valid tags parameter and valid sortBy", () => {
   });
 });
 
-describe("Test posts path with valid tags parameter and invalid sortBy", () => {
-  test("status code should be 400 with error message", async () => {
+describe('Test posts path with valid tags parameter and invalid sortBy', () => {
+  test('status code should be 400 with error message', async () => {
     const response = await request(app).get(
-      "/api/posts?tags=history,tech&sortBy=desca"
+      '/api/posts?tags=history,tech&sortBy=desca'
     );
     expect(response.statusCode).toBe(400);
     expect(response.body.error).toEqual(
-      expect.stringContaining("sortBy parameter is invalid")
+      expect.stringContaining('sortBy parameter is invalid')
     );
   });
 });
 
-describe("Test posts path with valid tags parameter, valid sortBy and valid direction", () => {
-  test("status code should be 200 with posts sorted by likes in desc order", async () => {
+describe('Test posts path with valid tags parameter, valid sortBy and valid direction', () => {
+  test('status code should be 200 with posts sorted by likes in desc order', async () => {
     const response = await request(app).get(
-      "/api/posts?tags=history,tech&sortBy=likes&direction=desc"
+      '/api/posts?tags=history,tech&sortBy=likes&direction=desc'
     );
     expect(response.statusCode).toBe(200);
     expect(response.body.posts[0].likes).toEqual(985);
@@ -67,14 +67,14 @@ describe("Test posts path with valid tags parameter, valid sortBy and valid dire
   });
 });
 
-describe("Test posts path with valid tags parameter, valid sortBy and invalid direction", () => {
-  test("status code should be 200 with posts sorted by likes in desc order", async () => {
+describe('Test posts path with valid tags parameter, valid sortBy and invalid direction', () => {
+  test('status code should be 200 with posts sorted by likes in desc order', async () => {
     const response = await request(app).get(
-      "/api/posts?tags=history,tech&sortBy=likes&direction=desca"
+      '/api/posts?tags=history,tech&sortBy=likes&direction=desca'
     );
     expect(response.statusCode).toBe(400);
     expect(response.body.error).toEqual(
-      expect.stringContaining("direction parameter is invalid")
+      expect.stringContaining('direction parameter is invalid')
     );
   });
 });
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import blogPosts from "./api/blog-posts.route.js";
+import express from 'express';
+import blogPosts from './api/blog-posts.route.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", blogPosts);
-app.use("*", (req, res) => {
-  res.status(404).json({ error: "not found" });
+app.use('/api', blogPosts);
+app.use('*', (req, res) => {
+  res.status(404).json({ error: 'not found' });
 });
 
 export default app;
